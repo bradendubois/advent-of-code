033c1917cc38952e85cc7612d9debb247eb4c472
Braden <drab3n@gmail.com>
2019-12-17 20:12:11 -0600

    Added 2015 Day 10

---
+++ 2015/day10/10-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> lookandsay(vector<int> in) {
+    vector<int> newVec;
+    int prev = in.front();
+    int run = 0;
+    for (auto i: in) {
+        if (i == prev) run++;
+        else {
+            newVec.push_back(run);
+            newVec.push_back(prev);
+            run = 1;  
+        } prev = i;
+    } 
+    newVec.push_back(run);
+    newVec.push_back(prev);
+    return newVec;
+}
+
+int main() {
+    vector<int> vec{1, 1, 1, 3, 2, 2, 2, 1, 1, 3};
+    int n = 50;
+    while (n--) vec = lookandsay(vec);
+    cout << vec.size() << endl;
+}
