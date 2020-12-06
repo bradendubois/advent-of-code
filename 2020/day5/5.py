2a979ff9e3ce0a1cfe899e6f156fd51e00d15a0e
Braden Dubois <drab3n@gmail.com>
2020-12-05 19:02:40 -0600

    Simplified 2020 Day 5

--- 2020/day5/5.py
+++ 2020/day5/5.py
@@ -6,22 +6,13 @@ for line in stdin:
 
     l.append(line.rstrip())
 
-
-def bsp(low, high, low_c, s):
-
-    mid = (high + low) // 2
-
-    if low == high:
-        return low
-
-    else:
-        r = (low, mid) if s[0] == low_c else (mid + 1, high)
-        return bsp(*r, low_c, s[1:])
-
 seats = set()
 
 for boarding_pass in l:
-    r, c = bsp(0, 127, "F", boarding_pass[:-3]), bsp(0, 7, "L", boarding_pass[-3:])
+
+    r = int(boarding_pass[:-3].replace("F", '0').replace("B", '1'), 2)
+    c = int(boarding_pass[-3:].replace("L", '0').replace("R", '1'), 2)
+
     seats.add(r * 8 + c)
 
 low = min(seats)
