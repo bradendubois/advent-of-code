e865ded8106e10bc1ae83b79cb4a8b0b32d7826f
Braden Dubois <drab3n@gmail.com>
2020-12-30 23:29:34 -0600

    Refactored into submodules

--- 2018/day1/1-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    int x, sum = 0;
-    while (cin >> x) sum += x;
-    cout << sum << endl;
-}
--- 2019/day1/1-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    int x, sum=0;
-    while (cin >> x) sum += ((x / 3) - 2);
-    cout << sum << endl;
-}
